8e83f1c137754a2df1c2e5e73749db0278e710dd
Alex Kaul <alexk111@users.noreply.github.com>
2020-01-06 17:38:46 +0700

    Fix antd issues when client builds

--- @app/client/config-overrides.js
+++ @app/client/config-overrides.js
@@ -23,8 +23,6 @@ const themeVariables = lessToJS(
 module.exports = override(
   fixBabelImports("import", {
     libraryName: "antd",
-    libraryDirectory: "es",
-    style: true,
   }),
   addLessLoader({
     javascriptEnabled: true,
